abff9aee7ca4aac05b1193ebc326644bfdd5e791
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-09-09 22:19:56 +0530

    done using recusrsion

---
+++ Recursion/printnumbers.java
@@ -0,0 +1,20 @@
+public class printnumbers{
+    // public static void numbers(int i,int n){
+    //     if(i>n){
+    //         return;
+    //     }
+    //     System.out.println(i);
+    //     numbers(i+1,n);
+    // }
+
+    // public static void numbers(int i,int n){
+    //     if(n<i){
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     numbers(1,n-1);
+    // }
+    public static void main(String[]args){
+        numbers(1,10);
+    }
+}
