fd4e77cf8b5cd9e07837c604b5d14bffe5551fb1
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-09-10 23:31:03 +0530

    done sum using recursion

---
+++ Recursion/sumrecursion.java
@@ -0,0 +1,13 @@
+public class sumrecursion{
+    public static int numbers(int n){
+        
+        if(n<=1){  
+            return n;   
+        }
+        
+        return n+ numbers(n-1);
+    }
+    public static void main(String[]args){
+       System.out.println(numbers(10));
+    }
+}
